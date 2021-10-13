5ef5c70e4dfc4bf88f1fecf940237b523cc543a2
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2021-10-13 12:56:42 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
+    
   ],
   imports: [
     BrowserModule
