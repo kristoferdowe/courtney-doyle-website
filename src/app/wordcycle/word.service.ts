76ac614e4f3581c1bc82d4f04ad6554fb6b69726
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2021-12-31 10:45:26 -0500

    changed styling for word components, changed contact form style

--- src/app/wordcycle/word.service.ts
+++ src/app/wordcycle/word.service.ts
@@ -9,14 +9,27 @@ import { WordItem } from './word-item';
 export class WordService {
   getWords() {
     return [
-      new WordItem(WordListComponent, { word: 'health' }),
+      
 
-      new WordItem(WordListComponent, { word: 'wellness' }),
+      new WordItem(WordListComponent, { word: 'reiki' }),
 
-      new WordItem(WordListComponent, { word: 'strength' }),
+      new WordItem(WordListComponent, { word: 'yoga therapy' }),
 
-      new WordItem(WordListComponent, { word: 'balance' }),
+      new WordItem(WordListComponent, { word: 'Y12SR' }),
 
+      new WordItem(WordListComponent, { word: 'coaching' }),
+
+      new WordItem(WordListComponent, { word: 'holistic work' }),
+
+      new WordItem(WordListComponent, { word: 'private yoga' }),
+
+      new WordItem(WordListComponent, { word: 'meditation' }),
+
+      new WordItem(WordListComponent, { word: 'breathwork' }),
+
+      new WordItem(WordListComponent, { word: 'mindfulness' }),
+
+       
    
     ];
   }
